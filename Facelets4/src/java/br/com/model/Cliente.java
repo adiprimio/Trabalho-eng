cbd4b46d60826fb6de6872d824285c4312eb9e11
anibal di primio <adiprimio@gmail.comgit initgit inittouch teste.txtgit addgit commit -m primeiro>
2013-04-08 18:41:11 -0300

    primeiro

---
+++ Facelets4/src/java/br/com/model/Cliente.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.model;
+
+/**
+ *
+ * @author Magnata-PC
+ */
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private int idade;
+    private String sexo;
+
+    public Cliente(){
+        
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+}
